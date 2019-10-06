a60407b75508977610d5a56db5a7eeb0891f5a95
Tim Akermann <ti.akermann@gmail.com>
2019-10-06 16:58:17 +0200

    Added More Build Configurations.

---
+++ MinorGame/scenes/EmptyTestScene.cs
@@ -0,0 +1,13 @@
+﻿using MinorEngine.engine.components;
+using MinorEngine.engine.core;
+
+namespace MinorGame.scenes
+{
+    public class EmptyTestScene : AbstractScene
+    {
+        protected override void InitializeScene()
+        {
+            GameEngine.Instance.World.Add(DebugConsoleComponent.CreateConsole());
+        }
+    }
+}
