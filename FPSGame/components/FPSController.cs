00c5cec1cc9553c8c39cfbb2241765f8e49eb61c
Tim Akermann <ti.akermann@gmail.com>
2019-12-01 10:13:54 +0100

    Added FPS Controller Movement. Created Ground with texture to move on. Updated Engine Version.

--- FPSGame/components/FPSController.cs
+++ FPSGame/components/FPSController.cs
@@ -1,18 +1,27 @@
 ﻿using System;
 using Engine.Core;
 using Engine.Debug;
-using Engine.Physics.BEPUutilities;
+using Engine.Physics;
+using OpenTK;
 using MathHelper = OpenTK.MathHelper;
+using Vector3 = Engine.Physics.BEPUutilities.Vector3;
 
 namespace FPSGame.components
 {
     public class FPSController : AbstractComponent
     {
+        private Collider c;
         private OpenTK.Vector2 wpos = GameEngine.Instance.WindowPosition;
         private float pitch;
         private float sensitivity = 0.1f;
         private float yaw;
         private bool firstUpdate = true;
+
+        protected override void Awake()
+        {
+            c = Owner.GetComponent<Collider>();
+        }
+
         protected override void Update(float deltaTime)
         {
             if (firstUpdate)
@@ -26,7 +35,7 @@ namespace FPSGame.components
 
                 if (GameEngine.Instance.HasFocus)
                 {
-                    OpenTK.Input.Mouse.SetPosition(windowCenter.X+8, windowCenter.Y+31); //Magic Numbers because windows thinks that window borders and frames are not counting towards the window size :^)
+                    OpenTK.Input.Mouse.SetPosition(windowCenter.X + 8, windowCenter.Y + 31); //Magic Numbers because windows thinks that window borders and frames are not counting towards the window size :^)
                 }
                 OpenTK.Vector2 delta = screenMousePos - windowCenter;
                 Logger.Log("Delta: " + delta, DebugChannel.Log, 10);
@@ -51,9 +60,32 @@ namespace FPSGame.components
             front.Y = (float)Math.Sin(MathHelper.DegreesToRadians(pitch));
             front.Z = (float)Math.Cos(MathHelper.DegreesToRadians(pitch)) * (float)Math.Sin(MathHelper.DegreesToRadians(yaw));
             front = Vector3.Normalize(front);
-
-
+            front += Owner.LocalPosition;
             Owner.LookAt(front);
+        }
+
+        protected override void OnKeyPress(object sender, KeyPressEventArgs e)
+        {
+            Vector3 v = new Vector3();
+            if (e.KeyChar == 'w')
+            {
+                v = (-Vector4.UnitZ * Owner.GetWorldTransform()).Xyz;
+            }
+            else if(e.KeyChar == 'a')
+            {
+                v = (-Vector4.UnitX * Owner.GetWorldTransform()).Xyz;
+            }
+            else if (e.KeyChar == 's')
+            {
+                v = (Vector4.UnitZ * Owner.GetWorldTransform()).Xyz;
+            }
+            else if(e.KeyChar == 'd')
+            {
+                v = (Vector4.UnitX * Owner.GetWorldTransform()).Xyz;
+            }
+
+            v *= 10;
+            c.SetVelocityLinear(v);
         }
     }
 }
--- FPSGame/scenes/GameTestScene.cs
+++ FPSGame/scenes/GameTestScene.cs
@@ -1,7 +1,17 @@
-﻿using System.Drawing;
+﻿using System.Collections.Concurrent;
+using System.Diagnostics;
+using System.Drawing;
 using Engine.Core;
 using Engine.DataTypes;
+using Engine.Debug;
 using Engine.IO;
+using Engine.OpenCL;
+using Engine.OpenCL.DotNetCore.Memory;
+using Engine.OpenCL.TypeEnums;
+using Engine.OpenFL;
+using Engine.Physics;
+using Engine.Physics.BEPUphysics.Entities.Prefabs;
+using Engine.Physics.BEPUphysics.Materials;
 using Engine.Rendering;
 using FPSGame.components;
 using OpenTK;
@@ -10,25 +20,70 @@ namespace FPSGame.scenes
 {
     public class GameTestScene : AbstractScene
     {
+        private Texture GenerateMenuBackground()
+        {
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+            int texWidth = 128;
+            int texHeight = 128;
+            Interpreter i = new Interpreter(CLAPI.MainThread, "assets/filter/game/grass.fl", DataTypes.UCHAR1, CLAPI.CreateEmpty<byte>(CLAPI.MainThread, texWidth * texHeight * 4, MemoryFlag.ReadWrite), texWidth, texHeight, 1, 4, "assets/kernel/", true);
+
+            do
+            {
+                i.Step();
+            } while (!i.Terminated);
+
+            Texture tex = TextureLoader.ParameterToTexture(texWidth, texHeight);
+            TextureLoader.Update(tex, i.GetResult<byte>(), (int)tex.Width, (int)tex.Height);
+            Logger.Log("Time for Menu Background(ms): " + sw.ElapsedMilliseconds, DebugChannel.Log, 10);
+            sw.Stop();
+            return tex;
+        }
+
         protected override void InitializeScene()
         {
+            LayerManager.RegisterLayer("physics", new Layer(0, 1));
+
             BasicCamera mainCamera =
                 new BasicCamera(
                     Matrix4.CreatePerspectiveFieldOfView(MathHelper.DegreesToRadians(75f),
                         GameEngine.Instance.Width / (float)GameEngine.Instance.Height, 0.01f, 1000f), Vector3.Zero);
 
             object mc = mainCamera;
+
             SetCamera(mainCamera);
             Add(mainCamera);
             mainCamera.AddComponent(new FPSController());
-
+            mainCamera.LocalPosition = new Engine.Physics.BEPUutilities.Vector3(0, 3, 0);
+            Collider playerColl = new Collider(new Sphere(new Engine.Physics.BEPUutilities.Vector3(0, 3, 0), 1f, 1f), "physics");
+            mainCamera.AddComponent(playerColl);
+            playerColl.UpdateRotation = false;
+            playerColl.ColliderConstraints = new ColliderConstraints { RotationConstraints = FreezeConstraints.ALL };
+            playerColl.PhysicsCollider.Material = new Material(1.5f, 1.5f, 0);
+            playerColl.PhysicsCollider.LinearDamping = 0.5f;
             for (int i = 0; i < 50; i++)
             {
-                GameObject obj = new GameObject(new Vector3(i * 2 - 50, 0, -10), "Cube" + i);
+                GameObject obj = new GameObject(new Vector3(i * 2 - 50, 3, -10), "Cube" + i);
                 LitMeshRendererComponent lmr = new LitMeshRendererComponent(DefaultFilepaths.DefaultLitShader, Prefabs.Cube, TextureLoader.ColorToTexture(Color.Red), 1);
+                Collider bcoll = new Collider(new Box(Engine.Physics.BEPUutilities.Vector3.Zero, 1, 1, 1, 1f), "physics");
+                bcoll.PhysicsCollider.Material = new Material(10, 10, 0);
+
                 obj.AddComponent(lmr);
+                obj.AddComponent(bcoll);
                 Add(obj);
             }
+
+            GameObject ground = new GameObject("Ground");
+            LitMeshRendererComponent groundRenderer = new LitMeshRendererComponent(DefaultFilepaths.DefaultLitShader, Prefabs.Cube, GenerateMenuBackground(), 1);
+            ground.AddComponent(groundRenderer);
+            Collider coll = new Collider(new Box(Engine.Physics.BEPUutilities.Vector3.Zero, 100, 100, 100), "physics");
+            coll.PhysicsCollider.Material = new Material(10, 10, 0);
+            ground.AddComponent(coll);
+            Add(ground);
+            ground.Scale = new Engine.Physics.BEPUutilities.Vector3(50, 50, 50);
+            ground.LocalPosition = new Engine.Physics.BEPUutilities.Vector3(0, -50, 0);
+
+
 
         }
     }
